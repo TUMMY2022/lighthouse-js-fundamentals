67d70d05af1b11a837918d28315f3e6e08f28b9c
Tomy <to88my@gmail.com>
2022-12-20 01:15:36 +0000

    commit return hello name

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
 
+
+/*
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
